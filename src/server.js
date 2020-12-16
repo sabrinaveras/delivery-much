aee9472f93ef783c2265cb14e2ba7dda257e3943
Veras <sabrina.alves@drcash.com.br>
2020-12-16 13:55:46 -0300

    remove the code and start the implementation

--- src/server.js
+++ src/server.js
@@ -1,70 +1,15 @@
-const express = require("express");
-const axios = require("axios");
-const giphy = require("giphy-api")("TY6YhWEJ6b2Bci9M74PTMj2DeQebGgpv");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
+require("dotenv").config()
 
-async function getGif(title){
+const app = express()
 
-    return await giphy.random(title)
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-}
-
-
-app.get("/", async (request, response) => {
-
-    let finalResult = [];
-    let ingredients = request.query.i.split(",").sort();
-
-    console.log("Ingredients: ", ingredients)
-
-    if(ingredients.length > 3)
-        return response.status(400).json({ error: "muitos ingredientes, escolha somente três"})
-
-
-    await axios.get("http://www.recipepuppy.com/api/?i=" + request.query.i)
-        .then(axiosResponse => {
-
-            if(axiosResponse.data.results.length){
-                if(axiosResponse.status !== 200){
-                    return response.status(400).json({ error: "Problemas em conseguir suas receitas com esses ingredientes"})
-                }
-
-                axiosResponse.data.results.forEach(function (result) {
-
-                    finalResult.push( {
-                        "title": result.title,
-                        "ingredients": result.ingredients,
-                        "link": result.href
-                    })
-
-                })
-
-                giphy.trending({tag: "superman", limit: 10}, function (error, giphyResponse){
-                    console.log("response: ", giphyResponse.data);
-                })
-
-                return response.status(200).json({keywords: ingredients, recipes: finalResult})
-            }else{
-                return response.status(404).json({ error: "não tenhos receitas com esse ingrediente"})
-            }
-
-
-        }).catch(error => {
-            console.error(error)
-    })
-
-
-    // giphy.random('pokemon', function (err, res) {
-    //     console.log(res.data.url);
-    //
-    //     // return response.status(200).send(res.data)
-    //     return response.status(200).send("<img src={res.data.url} alt={'giphy'}/>")
-    // });
-
-})
-
-
+// routes
 
 
 app.listen(3333, () => {
